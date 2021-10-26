374f55b6409dc09d76402f9d5f549da930cfe334
alok2110 <aloksaxena755@gmail.com>
2021-10-26 18:30:11 +0530

    new

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static("public"));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
