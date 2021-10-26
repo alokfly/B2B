ee3b4cd7b8597edcb7b6176c414e23a1e585e630
alok2110 <aloksaxena755@gmail.com>
2021-10-26 13:44:34 +0530

    new change

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var cors = require("cors");
 require("dotenv").config();
 const app = express();
 
+app.use(cors());
+
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
